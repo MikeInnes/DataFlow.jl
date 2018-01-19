479012a2369d34359b0ad86012c57139bcc1b02b
Mike J Innes <mike.j.innes@gmail.com>
2018-01-19 11:43:43 +0000

    more general indexing

--- src/graph/graph.jl
+++ src/graph/graph.jl
@@ -20,10 +20,11 @@ thread!(v::Vertex, xs...) = foldl(thread!, v, xs)
 
 (::Type{T}){T<:Vertex}(x, args...) = thread!(T(x), args...)
 
-Base.getindex(v::Vertex, i::Integer) = inputs(v)[i]
-Base.getindex(v::Vertex, i::Integer, is::Integer...) = v[i][is...]
-Base.setindex!(v::Vertex, x::Vertex, i::Integer) = v.inputs[i] = x
-Base.setindex!(v::Vertex, x::Vertex, i::Integer, is...) = v.inputs[i][is...] = x
+Base.getindex(v::Vertex, i) = inputs(v)[i]
+Base.getindex(v::Vertex, i, is...) = v[i][is...]
+Base.setindex!(v::Vertex, x::Vertex, i) = v.inputs[i] = x
+Base.setindex!(v::Vertex, x::Vertex, i, is...) = v.inputs[i][is...] = x
+Base.endof(v::Vertex) = endof(v.inputs)
 
 function collectv(v::Vertex, vs = OASet{typeof(v)}())
   v ∈ vs && return collect(vs)
